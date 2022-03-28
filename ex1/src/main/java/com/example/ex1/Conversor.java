be26a5879ae4038c646d6d27a4c70cc0a2646111
Kauana Henriques <karoltvbl@gmail.com>
2022-03-27 21:36:12 -0300

    refactor

--- ex1/src/main/java/com/example/ex1/Conversor.java
+++ ex1/src/main/java/com/example/ex1/Conversor.java
@@ -2,10 +2,10 @@ package com.example.ex1;
 
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,20 +14,19 @@ import javax.ws.rs.core.MediaType;
 public class Conversor {
     
 	@GET
-	@Path("/km")
+	@Path("/km/{km}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public String ConverteParaMilha(){
-		double km = 10.0;
+	public String ConverteParaMilha(@PathParam("km") double km){
 		double milha = km / 1.609;
         String resultado = Double.toString(milha);		
 		return resultado;
 	}
 	@POST
-	@Path("/kn")
+	@Path("/kn/{kn}")
 	@Produces(MediaType.APPLICATION_FORM_URLENCODED)
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Object ConverteParaKm(@FormParam("kn") final double kn){
+	public Object ConverteParaKm(@PathParam("kn") double kn){
 		double km  = (kn / 1.852);
         Object resultado = km;
 		return resultado;
